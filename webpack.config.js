fbee4840cc8223f722d3d931ade0286d2b94f753
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-12-19 21:42:10 +0530

    Initial Setup.

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const { dirname } = require('path')
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/index.js",
+    output: {
+        filename: 'main.js',
+        path: path.resolve(--dirname, 'dist'),
+    },
+}
