76900201271cefd508b29a7c38bba4c688ccd7ad
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-12-22 00:05:38 +0530

    Updated Header Markup and Styling

---
+++ src/modules/primary.js
@@ -0,0 +1,9 @@
+import Task from "./task";
+
+export default class UI {
+  static loadHomePage() {
+    //static method : a method defined directly on a class and can be called without creating an instance of the class
+
+    
+  }
+}
--- src/modules/task.js
+++ src/modules/task.js
@@ -1,6 +1,8 @@
-export default class Task { // Class : a blueprint for creating objects. Objects created from class are called Instance of that class.
-  constructor(name, dueDate = "No Date") { // called when a new instance of the class is created. i.e. an object is created.
-    this.name = name;  // this keyword refers to the current instance of the class.
+export default class Task {
+  // Class : a blueprint for creating objects. Objects created from class are called Instance of that class.
+  constructor(name, dueDate = "No Date") {
+    // called when a new instance of the class is created. i.e. an object is created.
+    this.name = name; // this keyword refers to the current instance of the class.
     this.dueDate = dueDate;
   }
 
@@ -31,4 +33,3 @@ export default class Task { // Class : a blueprint for creating objects. Objects
 
 // task.setDate('Tomorrow');
 // console.log(task.getDate()); // prints 'Tomorrow'
-
