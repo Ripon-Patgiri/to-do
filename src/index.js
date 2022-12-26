1b4d437b14235f2a82839a6947497f8e51dc2a7b
Ripon-Patgiri <riponmongzeell@gmail.com>
2022-12-26 22:46:56 +0530

    functionality to input tasks from user and display it on lists area.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ const listsContainer = document.querySelector("[data-lists]"); // To select the 
 const newListForm = document.querySelector("[data-new-list-form"); // Select the new list form
 const newListInput = document.querySelector("[data-new-list-input"); // Selects the input area
 
-
 let lists = [
   { id: 1, name: "name" },
   { id: 2, name: "todo" },
 ]; //variable to hold all of our lists
 
-newListForm.addEventListener('submit', e => {
-    e.preventDefault();
-})
+newListForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const listName = newListInput.value;
+  if (listName == null || listName === "") return;
+  const list = createList(listName); // passing to a function to add the name to the list
+  newListInput.value = null;
+  lists.push(list);
+  render();
+});
+
+function createList(name) {
+  return { id: Date.now().toString(), name: name, tasks: [] };
+}
 
 function render() {
   // function to render items in the lists area
